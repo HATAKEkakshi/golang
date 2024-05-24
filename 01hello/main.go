bfdf5b3539a2799b1709f13886641814b2dd2203
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-05-24 22:39:01 +0530

    testing go added some changes

--- 01hello/main.go
+++ 01hello/main.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello from hemant kumar")
 	fmt.Println("Your are an noob programmer wanna be pro so partice hard as you can")
 	fmt.Println("yes i gonna practice as hard as i can")
+	fmt.Println("testing go ")
 }
