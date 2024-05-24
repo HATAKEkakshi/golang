24b0cd10485a166ba9bf3215482a883b40a3b6b0
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-05-24 22:12:39 +0530

    go lang basic

---
+++ 01hello/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello from hemant kumar")
+	fmt.Println("Your are an noob programmer wanna be pro so partice hard as you can")
+	fmt.Println("yes i gonna practice as hard as i can")
+}
