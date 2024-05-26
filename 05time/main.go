73577410cc622ffedec3a943a0a397dbed8cfe69
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-05-26 14:13:09 +0530

    time

--- 05time/main.go
+++ 05time/main.go
@@ -12,4 +12,7 @@ func main() {
 	fmt.Println(presentTime)
 
 	fmt.Println(presentTime.Format("01-02-2000 15:04:05 Wednesday"))
+
+	createdDate := time.Date(2020, time.August, 10, 23, 23, 0, 0, time.UTC)
+	fmt.Println(createdDate)
 }
