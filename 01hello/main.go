a1bb6cb1ffa10d5666eb419f4dfc2f87c9d303a0
HATAKEkakashi <hemant.kumar40fea@gmail.com>
2024-05-26 06:14:33 +0530

    main go

--- 01hello/main.go
+++ 01hello/main.go
@@ -7,4 +7,5 @@ func main() {
 	fmt.Println("Your are an noob programmer wanna be pro so partice hard as you can")
 	fmt.Println("yes i gonna practice as hard as i can")
 	fmt.Println("testing go ")
+	fmt.Println("testing the lexer")
 }
